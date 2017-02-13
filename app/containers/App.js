42de45ce9f00e619f70787ce66e6d5f804404542
Carter J. Bastian <carter.bastian1@gmail.com>
2017-02-13 17:07:58 -0500

    Add react-raphael imports to a React container.

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,5 +1,8 @@
 // @flow
 import React, { Component } from 'react';
+var ReactDOM = require('react-dom');
+
+const {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('react-raphael');
 
 export default class App extends Component {
   props: {
