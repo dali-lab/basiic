a3c51b132468ec92c72a80e7a6cae7916fce96b3
Carter J. Bastian <carter.bastian1@gmail.com>
2017-02-16 18:17:56 -0500

    Get window Height and Width in app container

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 var ReactDOM = require('react-dom');
+var electron = require('electron');
 
 const {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('react-raphael');
 
@@ -10,19 +11,27 @@ export default class App extends Component {
   };
 
   render() {
+    // Get the size of the app window in the component
+    var primaryDisplay = electron.screen.getPrimaryDisplay();
+    var windowHeight = primaryDisplay.size.height;
+    var windowWidth = primaryDisplay.size.width;
+    
+    console.log("Height: %d", windowHeight);
+    console.log("Width: %d", windowWidth);
+
     // Create the list of dots to add to the dot-grid
-    var data = [];
+    var dotGrid = [];
     var i, j;
     for (i = 5; i < 300; i += 15)
       for (j = 5; j < 300; j += 15)
-        data.push({x:i, y:j, r:.5, attr:{"stroke":"#00cc00","stroke-width":1,"fill":"#00cc00"}});
+        dotGrid.push({x:i, y:j, r:.5, attr:{"stroke":"#00cc00","stroke-width":1,"fill":"#00cc00"}});
     
     return (
       <div>
         <Paper width={300} height={300}>
           <Set>    
             {
-                data.map(function(ele,pos){
+                dotGrid.map(function(ele,pos){
                     return (<Circle key={pos} x={ele.x} y={ele.y} r={ele.r} attr={ele.attr}/>)
                 })
             }
