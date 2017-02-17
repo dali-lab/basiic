de613ad85fb05a61a6a6fc8725b132011932ec37
Carter J. Bastian <carter.bastian1@gmail.com>
2017-02-16 19:16:53 -0500

    Create a full-window dot grid

--- app/containers/App.js
+++ app/containers/App.js
@@ -12,31 +12,29 @@ export default class App extends Component {
 
   render() {
     // Get the size of the app window in the component
+    // TODO(carter): This is the screen width and height. Figure out
+    // the width and height of this component.
     var primaryDisplay = electron.screen.getPrimaryDisplay();
     var windowHeight = primaryDisplay.size.height;
     var windowWidth = primaryDisplay.size.width;
     
-    console.log("Height: %d", windowHeight);
-    console.log("Width: %d", windowWidth);
-
     // Create the list of dots to add to the dot-grid
     var dotGrid = [];
     var i, j;
-    for (i = 5; i < 300; i += 15)
-      for (j = 5; j < 300; j += 15)
+    for (i = 5; i < windowWidth; i += 15)
+      for (j = 5; j < windowHeight; j += 15)
         dotGrid.push({x:i, y:j, r:.5, attr:{"stroke":"#00cc00","stroke-width":1,"fill":"#00cc00"}});
-    
+   
+    var test = 100;
     return (
       <div>
-        <Paper width={300} height={300}>
+        <Paper width={windowWidth} height={windowHeight}>
           <Set>    
             {
                 dotGrid.map(function(ele,pos){
                     return (<Circle key={pos} x={ele.x} y={ele.y} r={ele.r} attr={ele.attr}/>)
                 })
             }
-          </Set>
-          <Set>
           </Set>
         </Paper>
       </div>);
