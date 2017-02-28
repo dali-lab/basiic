97a719b2c0545cb04360f252260bf95f30a56946
Carter J. Bastian <carter.bastian1@gmail.com>
2017-02-28 17:39:06 -0500

    Build template for react layout
    The individual components are defined in this commit. The Canvas and ShapeBar
    components are positioned for the sake of getting the hang of CSS in JSX.

---
+++ app/components/Canvas.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+var ReactDOM = require('react-dom');
+var electron = require('electron');
+
+// const {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('react-raphael');
+
+export default class Canvas extends Component {
+  render() {
+    return (
+      <div style={{position: 'absolute', backgroundColor: 'blue', width: '100%', height: '90%', margin: 0, padding: 0, top: '10%'}}>
+        <div></div>
+      </div>);
+  }
+}
--- app/components/DotGrid.js
+++ app/components/DotGrid.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 var ReactDOM = require('react-dom');
 var electron = require('electron');
 
-import Canvas from '../components/Canvas'; 
-import ShapeBar from '../components/ShapeBar';
-
 // const {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('react-raphael');
 
 export default class App extends Component {
@@ -21,8 +18,7 @@ export default class App extends Component {
   render() {
     return (
       <div style={{position: 'absolute', backgroundColor: 'green', width: '100%', height: '100%', margin: 0, padding: 0}}>
-        <ShapeBar />
-        <Canvas />
+        <div></div>
       </div>);
   }
 }
