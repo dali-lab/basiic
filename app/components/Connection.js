feed67a51cdccc82d7a13aee3509a197e52c9b31
Carter J. Bastian <carter.bastian1@gmail.com>
2017-02-28 18:21:35 -0500

    Add skeleton for Shape and Connection components

---
+++ app/components/Connection.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+var ReactDOM = require('react-dom');
+var electron = require('electron');
+
+const {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('react-raphael');
+
+export default class Connection extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>);
+  }
+}
---
+++ app/components/Shape.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+var ReactDOM = require('react-dom');
+var electron = require('electron');
+
+onst {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('react-raphael');
+
+export default class Shape extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>);
+  }
+}
